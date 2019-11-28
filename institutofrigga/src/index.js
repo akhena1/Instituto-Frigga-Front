0467f433e0501431a6e9880ca3d72d10a06bb446
Nakkat <benaka4@gmail.com>
2019-11-28 11:15:56 -0200

    a

--- institutofrigga/src/index.js
+++ institutofrigga/src/index.js
@@ -5,13 +5,12 @@ import * as serviceWorker from './serviceWorker';
 import Home from './assets/pages/Home/Home';
 import Produto from './assets/pages/Produto/Produto';
 import Receita from './assets/pages/Receita/Receita';
-/* import VerReceita from './assets/pages/VerReceita/VerReceita'; */
+import VerReceita from './assets/pages/VerReceita/VerReceita'; 
 import Perfil from './assets/pages/Perfil/Perfil';
 import Entrar from './assets/pages/Entrar/Entrar';
 import About from './assets/pages/About/About';
 import NotFound from '../src/assets/pages/NotFound/NotFound'
-// Redirect
-import {Route, BrowserRouter as Router, Switch, } from 'react-router-dom';
+import {Route, BrowserRouter as Router, Switch, Redirect} from 'react-router-dom';
 import './assets/css/estilo.css';
 
  const Rotas = (
@@ -26,8 +25,8 @@ import './assets/css/estilo.css';
                 <Route path = "/receitas" component={Receita}/>
                 <Route path = "/receita" component={Receita}/>
                 
-                {/* <Route path = "/receita/" component={VerReceita}/>
-                <Route path = "/receitas/" component={VerReceita}/> */}
+                <Route path = "/receita/" component={VerReceita}/>
+                <Route path = "/receitas/" component={VerReceita}/>
 
                 <Route path = "/perfil" component={Perfil}/>
 
@@ -35,7 +34,7 @@ import './assets/css/estilo.css';
 
                 <Route path = "/about" component={About}/>
 
-                {/* <Route component={NotFound}/> */}
+                <Route component={NotFound}/>
             </Switch>
         </div>
     </Router>
