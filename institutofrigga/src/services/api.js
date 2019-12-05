1afcbdfba6fd6c4cfa04a9fa916f19162523b571
akhena1 <gu.marcondes2012@gmail.com>
2019-12-05 12:06:49 -0300

    login

--- institutofrigga/src/services/api.js
+++ institutofrigga/src/services/api.js
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
 const api = Axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://localhost:5001/api",
     headers: {
         "Content-Type" : "application/json",
         "Authorization" : "Bearer " + localStorage.getItem("usuario-frigga"),
